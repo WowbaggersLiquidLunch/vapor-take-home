be0af19ca59af10dbb30b5013e5e419637e65fd2
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-07-09 18:59:48 -0400

    use implicit return

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -6,8 +6,8 @@ final class UserController {
     /// - Parameter request: The requet for adding a user.
     /// - Returns: A future of the user.
     func create(_ request: Request) throws -> Future<User> {
-        return try request.content.decode(User.self).flatMap { user in
-            return user.save(on: request)
+        try request.content.decode(User.self).flatMap { user in
+            user.save(on: request)
         }
     }
 
@@ -15,15 +15,15 @@ final class UserController {
     /// - Parameter request: The request for fetching all users.
     /// - Returns: A future of an array of all users.
     func index(_ request: Request) throws -> Future<[User]> {
-        return User.query(on: request).all()
+        User.query(on: request).all()
     }
 
     /// Finds the user specified in the given request's parameter.
     /// - Parameter request: The request that specifies which user to look for.
     /// - Returns: A future of the user.
     func find(_ request: Request) throws -> Future<User> {
-        return try request.parameters.next(User.self).flatMap { user in
-            return request.future(user)
+        try request.parameters.next(User.self).flatMap { user in
+            request.future(user)
         }
     }
 
@@ -31,8 +31,8 @@ final class UserController {
     /// - Parameter request: The request that specifies which user to update.
     /// - Returns: A future of the user.
     func update(_ request: Request) throws -> Future<User> {
-        return try request.parameters.next(User.self).flatMap({ user -> EventLoopFuture<User> in
-            return try request.content.decode(User.self).flatMap { updatedUser -> EventLoopFuture<User> in
+        try request.parameters.next(User.self).flatMap({ user -> EventLoopFuture<User> in
+            try request.content.decode(User.self).flatMap { updatedUser -> EventLoopFuture<User> in
                 user.name = updatedUser.name
                 return user.update(on: request)
             }
@@ -43,8 +43,8 @@ final class UserController {
     /// - Parameter request: The request that specifies which user to delete.
     /// - Returns: A furute of the HTTP status of this `DELETE` operation.
     func delete(_ request: Request) throws -> Future<HTTPStatus> {
-        return try request.parameters.next(User.self).flatMap { user in
-            return user.delete(on: request)
+        try request.parameters.next(User.self).flatMap { user in
+            user.delete(on: request)
         }.transform(to: .noContent)
     }
 }
