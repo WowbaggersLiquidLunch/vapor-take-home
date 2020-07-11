c2251bf898e67d79133bd958001f5f352099a859
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-07-11 08:44:13 -0400

    comment out `middlewares.use(FileMiddleware.self)`
    Because there is no public files to serve. So it's not needed.

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,7 +21,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
-    middlewares.use(FileMiddleware.self) // Allows Vapor to skip application logic and return public files directly.
+//    middlewares.use(FileMiddleware.self) // Allows Vapor to skip application logic and return public files directly.
     services.register(middlewares)
     
     // Configure a PostgreSQL database
